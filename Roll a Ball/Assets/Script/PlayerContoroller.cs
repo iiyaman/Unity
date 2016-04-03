bfe44fdfe8e504d2e4975c739116d8d5b22e5542
iiyaman <n.iyama0709@gmail.com>
2016-04-04 01:15:05 +0900

    4.アイテムの回収まで終了

---
+++ Roll a Ball/Assets/Script/PlayerContoroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerContoroller : MonoBehaviour 
+{
+
+	// speedを制御する
+	public float speed = 10;
+
+	void FixedUpdate ()
+	{
+		//入力をxとzに代入
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		// 同一のGameObjectが持つRigidbodyコンポーネントを取得
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		// rigidbodyのx軸（横）とz軸（奥）に力を加える
+		//rigidbody.AddForce(x * 10, 0, z * 10);
+
+		// xとyにspeedを掛ける
+		rigidbody.AddForce(x * speed, 0, z * speed);
+	}
+}
